e452ae1b9aef938d95bf40fdfdd346a4d5e0df39
Dmarveltechnologies <125516598+Marvellous-Dee8580@users.noreply.github.com>
2024-09-28 16:18:46 +0100

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/Self-Service-App/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['framer-motion'], // Add framer-motion to external dependencies
+    }
+  }
 });
